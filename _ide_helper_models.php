8ae54079f5fd14973cd3f2b5edb15065465f2309
danhnt1105 <troll190599@gmail.com>
2021-09-03 01:13:54 +0700

    booking model and factory

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,58 +10,82 @@
  */
 
 
-namespace App\Models {
-	/**
-	 * App\Models\Bookable
-	 *
-	 * @property int $id
-	 * @property \Illuminate\Support\Carbon|null $created_at
-	 * @property \Illuminate\Support\Carbon|null $updated_at
-	 * @property string $title
-	 * @property string $description
-	 * @method static \Database\Factories\BookableFactory factory(...$parameters)
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable newModelQuery()
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable newQuery()
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable query()
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereCreatedAt($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereDescription($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereId($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereTitle($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereUpdatedAt($value)
-	 */
-	class Bookable extends \Eloquent
-	{
-	}
+namespace App\Models{
+/**
+ * App\Models\Bookable
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $title
+ * @property string $description
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Booking[] $bookings
+ * @property-read int|null $bookings_count
+ * @method static \Database\Factories\BookableFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bookable whereUpdatedAt($value)
+ */
+	class Bookable extends \Eloquent {}
 }
 
-namespace App\Models {
-	/**
-	 * App\Models\User
-	 *
-	 * @property int $id
-	 * @property string $name
-	 * @property string $email
-	 * @property \Illuminate\Support\Carbon|null $email_verified_at
-	 * @property string $password
-	 * @property string|null $remember_token
-	 * @property \Illuminate\Support\Carbon|null $created_at
-	 * @property \Illuminate\Support\Carbon|null $updated_at
-	 * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
-	 * @property-read int|null $notifications_count
-	 * @method static \Database\Factories\UserFactory factory(...$parameters)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
-	 * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
-	 * @method static \Illuminate\Database\Eloquent\Builder|User query()
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
-	 * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
-	 */
-	class User extends \Eloquent
-	{
-	}
+namespace App\Models{
+/**
+ * App\Models\Booking
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $from
+ * @property string $to
+ * @property int $bookable_id
+ * @property-read \App\Models\Bookable $bookable
+ * @method static \Database\Factories\BookingFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereBookableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereTo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereUpdatedAt($value)
+ */
+	class Booking extends \Eloquent {}
 }
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">
+            Check Availability
+        </h6>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from" class="form-label">From</label>
+                <input
+                    v-model="from"
+                    id="from"
+                    type="text"
+                    name="from"
+                    class="form-control form-control-sm"
+                    placeholder="Start date"
+                    @keyup.enter="check"
+                />
+            </div>
+            <div class="form-group col-md-6">
+                <label for="to" class="form-label">To</label>
+                <input
+                    v-model="to"
+                    id="to"
+                    type="text"
+                    name="to"
+                    class="form-control form-control-sm"
+                    placeholder="End date"
+                    @keyup.enter="check"
+                />
+            </div>
+        </div>
+        <button class="btn btn-secondary btn-block" @click="check">
+            Check
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: "",
+            to: ""
+        };
+    },
+
+    methods: {
+        check() {
+            console.log("Check");
+        }
+    }
+};
+</script>
+
+<style scoped>
+.form-label {
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: gray;
+    font-weight: bolder;
+}
+</style>
